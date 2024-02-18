e5aeb18e0a08e41643f57728c8c1df447c97c0ea
seifRashid <rseif1218@gmail.com>
2024-02-18 21:04:31 +0300

    My first comming in the revision of PHP

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Rashid</title>
+</head>
+<body>
+    <h1>This is a title</h1>
+    <?php 
+    
+    $name = 'Rashid Seif';
+    echo'Hello, my name is '. $name .'.';
+
+
+    ?>
+</body>
+</html>
